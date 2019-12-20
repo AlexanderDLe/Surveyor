fc46e777dd3a2eb0c0ba4d641dcbda2b8e6f646b
Alexander Le <iamxanofficial@gmail.com>
2019-12-20 15:30:04 -0800

    Implemented cookie session; prod and dev keys.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
-const app = express();
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const connectDB = require('./config/db');
 connectDB();
 require('./models/User');
 require('./services/passport');
+
+const app = express();
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
