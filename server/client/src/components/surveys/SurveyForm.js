a646b6c5c8c4ad960aeaa55706baf54d994cca33
AlexanderDLe <alexanderledev@gmail.com>
2019-12-28 21:17:55 -0800

    Survey Form Review fields and submit Survey action

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -6,17 +6,12 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
 import { Link } from 'react-router-dom';
-
-const FIELDS = [
-    { label: 'Survey Title', name: 'title' },
-    { label: 'Subject Line', name: 'subject' },
-    { label: 'Email Body', name: 'body' },
-    { label: 'Recipient List', name: 'emails' }
-];
+import validateEmails from '../../utils/validateEmails';
+import formFields from './formFields';
 
 class SurveyForm extends Component {
     renderFields() {
-        return _.map(FIELDS, ({ label, name }) => {
+        return _.map(formFields, ({ label, name }) => {
             return (
                 <Field
                     component={SurveyField}
@@ -33,8 +28,8 @@ class SurveyForm extends Component {
         return (
             <div>
                 <form
-                    onSubmit={this.props.handleSubmit(values =>
-                        console.log(values)
+                    onSubmit={this.props.handleSubmit(
+                        this.props.onSurveySubmit
                     )}
                 >
                     {this.renderFields()}
@@ -57,23 +52,19 @@ class SurveyForm extends Component {
 function validate(values) {
     const errors = {};
 
-    if (!values.title) {
-        errors.title = 'You must provide a title.';
-    }
-    if (!values.subject) {
-        errors.subject = 'You must provide a subject.';
-    }
-    if (!values.body) {
-        errors.body = 'You must provide a body.';
-    }
-    if (!values.emails) {
-        errors.emails = 'You must provide emails.';
-    }
+    errors.recipients = validateEmails(values.recipients || '');
+
+    _.each(formFields, ({ name }) => {
+        if (!values[name]) {
+            errors[name] = 'You must provide a value.';
+        }
+    });
 
     return errors;
 }
 
 export default reduxForm({
     validate,
-    form: 'surveyForm'
+    form: 'surveyForm',
+    destroyOnUnmount: false
 })(SurveyForm);
